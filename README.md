8e70234b8960c41a49cd81c50f2a8b7dcfce03bb
XICHEN24 <xichen24@illinois.edu>
2017-12-14 15:14:21 -0600

    Add the changes to the newly forked PPP

--- README.md
+++ README.md
@@ -1,7 +1,130 @@
-# Phenotype_Prediction_Pipeline
-Regression to infer an 'omic'-drug association
+# KnowEnG's Phenotype Prediction Pipeline
+This is the Knowledge Engine for Genomics (KnowEnG), an NIH BD2K Center of Excellence, Phenotype Prediction Pipeline.
 
-* User submits an ‘omic’ spreadsheet with samples as columns and genes as rows. 
-* User also submits a phenotype value for each sample.
-* System learns to predict phenotype from ‘omic’ data. (Regression).
-* Specific example: identification of best drug for a patient.
+This pipeline **predicts" the relative importance of a set of genes associated with a given phenotype.
+
+This pipeline supports two regression models: 
+
+| **Options**                                      | **Method**                           | **Parameters** |
+| ------------------------------------------------ | -------------------------------------| -------------- |
+| Elastic Net                                      | Elastic                              | elastic_net    |
+| Lasso                                            | Lasso                                | Lasso          |
+
+* * *
+## How to run this pipeline with our data
+* * *
+
+### 1. Clone the Phenotype_Prediction_Pipeline
+```
+ git clone https://github.com/KnowEnG/Phenotype_Prediction_Pipeline.git
+```
+ 
+### 2. Install the following (Ubuntu or Linux)
+  ```
+ apt-get install -y python3-pip
+ apt-get install -y libblas-dev liblapack-dev libatlas-base-dev gfortran
+ pip3 install numpy==1.11.1
+ pip3 install pandas==0.18.1
+ pip3 install scipy==0.18.0
+ pip3 install scikit-learn==0.17.1
+ apt-get install -y libfreetype6-dev libxft-dev
+ pip3 install matplotlib=1.4.2
+ pip3 install pyyaml
+ pip3 install knpackage
+```
+
+### 3. Change directory to Phenotype_Prediction_Pipeline
+
+```
+cd Phenotype_Prediction_Pipeline
+```
+
+### 4. Change directory to test
+
+```
+cd test
+```
+
+### 5. Create a local directory "run_dir" and place all the run files in it
+```
+make env_setup
+```
+
+### 6. Select and run a phenotype prediction option:
+  
+ * Run Elastic Net pipeline</br>
+  ```
+  make run_elastic_net
+  ```
+ 
+ * Run Lasso pipeline</br>
+ ```
+ make run_lasso
+ ```
+ 
+__***Follow steps 1-3 above then do the following:***__
+
+### * Create your results directory
+ 
+ ```
+ mkdir results_directory
+ ```
+ 
+### * Create run_parameters file (YAML Format)
+ 
+ Look for examples of run_parameters in:
+  ```
+  Phenotype_Prediction_Pipeline/data/run_files BENCHMARK_2_ElasticNet.yml
+  
+  Phenotype_Prediction_Pipeline/data/run_files BENCHMARK_1_Lasso.yml
+
+  ```
+ 
+### * Run
+Using Elastic net
+  ```
+ python3 ../src/phenotype_prediction.py -run_directory ./run_dir -run_file BENCHMARK_2_ElasticNet.yml
+  ```
+Using Lasso
+ ```
+ python3 ../src/phenotype_prediction.py -run_directory ./run_dir -run_file BENCHMARK_1_Lasso.yml
+ ```
+ 
+ * * *
+ ## Description of "run_parameters" file
+ * * *
+ 
+| **Key**                        | **Value**            | **Comments**                       |
+| -------------------------      | --------------       | ------------                       |
+| Method                         | elastic_net_predict          | scikit-learn.org   elastic-net     |
+| Method                         | lasso_predict                | scikit-learn.org   lasso           |
+| results_directory              | directory            | Directory to save the output files |
+| spreadsheet_name_full_path     | spreadsheet_name     | Input Gene Expression  data        |
+| response_name_full_path        | response_name        | Input Drug Response data           |
+| test_spreadsheet_name_full_path| test_spreadsheet_name| Input testing feature data         |
+| min_alpha                      | float number         | Minimum number in alpha list       |
+| max_alpha                      | float number         | Maximum number in alpha list       |
+| tolerance                      | float number         | The tolerance for the optimization |
+| fit_intercept                  | boolean value        | whether to calculate the intercept for this model | 
+| normalize                      | boolean value        | whether the regressors will be normalized |
+| max_iter                       | integer number       | The maximum number of iterations   |
+| n_alpha                        | integer number       | Number of alphas in alpha list     |
+| min_l1                    | float number       | Minimum l1 in the grid of l1|
+| max_l1                    | float number         | Maximum l1 in the grid of l1|
+| n_l1                      | integer number       | Length of grid of l1           | 
+| eps                       | float number     | Length of the path|
+
+spreadsheet_name = features_train_clean.df</br>
+response_name = response_train_clean.df</br>
+test_spreadsheet_name = features_test_clean.df
+
+ * * * 
+ ## Description of Output files are saved in results directory
+ * * * 
+ 
+ | **Gene Name** | **Relarive Importance**|
+ | ------------- | ---------------------- |
+ | User Gene 1   | Float                  |
+ | ...           | ...                    |
+ | User Gene n   | Float                  |
+ 
--- src/phenotype_prediction_toolbox.py
+++ src/phenotype_prediction_toolbox.py
@@ -1,30 +1,104 @@
-# this is the toolbox of PPP
+
 import os
 import pandas as pd
-
-from sklearn import linear_model
+import numpy as np
+import pickle
+from   sklearn import linear_model
 
 import knpackage.toolbox as kn
 
+def run_lasso_predict(run_parameters):
+
+    gene_file = run_parameters['spreadsheet_name_full_path'     ]
+    sign_file = run_parameters['response_name_full_path'        ]
+    test_file = run_parameters['test_spreadsheet_name_full_path']
+
+    gene_df   = kn.get_spreadsheet_df(gene_file)
+    sign_df   = kn.get_spreadsheet_df(sign_file)
+    test_df   = kn.get_spreadsheet_df(test_file)
+
+    row_names = test_df.columns
+
+    gene_mat  = gene_df.values
+    sign_mat  = sign_df.values[0]
+    test_mat  = test_df.values
+
+    min_alpha     = run_parameters['min_alpha']
+    max_alpha     = run_parameters['max_alpha']
+    n_alpha       = run_parameters['n_alpha']
+    intercept     = run_parameters['fit_intercept']
+    normalization = run_parameters['normalize']
+    max_iter      = run_parameters['max_iter']
+    tolerance     = run_parameters['tolerance']
+
+    alpha_grid    = np.linspace(min_alpha, max_alpha, num=n_alpha)
+    reg_model     = linear_model.LassoCV(
+        alphas=alpha_grid, fit_intercept=intercept, \
+        normalize=normalization, max_iter=max_iter, tol=tolerance, cv=5)
+
+    reg_model.fit( gene_mat.T, sign_mat)
+    filename      = os.path.join(run_parameters['results_directory'], 'lasso_model.pkl') 
+    pickle.dump(reg_model, open(filename, 'wb'))
+    response_predict   = reg_model.predict(test_mat.T)
+    predict_df         = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
+    write_predict_data(predict_df, run_parameters)
+    
+
 def run_elastic_predict(run_parameters):
 
-    gene_samples_train_df = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'])
-    response_train_df = kn.get_spreadsheet_df(run_parameters['response_name_full_path'])
-    gene_samples_test_df = kn.get_spreadsheet_df(run_parameters['test_spreadsheet_name_full_path'])
-    response_test_sample_names = list(gene_samples_test_df.columns)
+    gene_file = run_parameters['spreadsheet_name_full_path'     ]
+    sign_file = run_parameters['response_name_full_path'        ]
+    test_file = run_parameters['test_spreadsheet_name_full_path']
 
-    reg_moE = linear_model.ElasticNetCV()
-    response_predict = reg_moE.fit(gene_samples_train_df.transpose().values,
-                                   response_train_df.values[0]).predict(gene_samples_test_df.transpose().values)
+    gene_df   = kn.get_spreadsheet_df(gene_file)
+    sign_df   = kn.get_spreadsheet_df(sign_file)
+    test_df   = kn.get_spreadsheet_df(test_file)
 
-    predict_df = pd.DataFrame(response_predict.T, index=response_test_sample_names, columns=['predict'])
+    row_names = test_df.columns
+
+    gene_mat  = gene_df.values
+    sign_mat  = sign_df.values[0]
+    test_mat  = test_df.values
+
+    eps       = run_parameters['eps']    
+    min_alpha = run_parameters['min_alpha']
+    max_alpha = run_parameters['max_alpha']
+    n_alpha   = run_parameters['n_alpha']
+    min_l1    = run_parameters['min_l1']
+    max_l1    = run_parameters['max_l1']
+    n_l1      = run_parameters['n_l1']
+    intercept = run_parameters['fit_intercept']
+    normalize = run_parameters['normalize']
+    max_iter      = run_parameters['max_iter']
+    tolerance     = run_parameters['tolerance']
+
+    alpha_grid= np.linspace(min_alpha, max_alpha, num=n_alpha)
+    l1_grid   = np.linspace(min_l1, max_l1, num=n_l1)
+
+
+    reg_model = linear_model.ElasticNetCV(
+        l1_ratio=l1_grid, alphas=alpha_grid, fit_intercept=intercept, eps = eps,\
+        normalize=normalize, max_iter=max_iter, tol=tolerance, cv=5)
+
+    reg_model.fit(gene_mat.T, sign_mat)
+
+    filename      = os.path.join(run_parameters['results_directory'], 'elastic_net_model.pkl') 
+    pickle.dump(reg_model, open(filename, 'wb'))
+    response_predict = reg_model.predict(test_mat.T)
+    predict_df        = pd.DataFrame(response_predict.T, index=row_names, columns=['predict'])
     write_predict_data(predict_df, run_parameters)
 
 
 def write_predict_data(predict_df, run_parameters):
-    deNada, output_file_name = os.path.split(run_parameters['test_spreadsheet_name_full_path'])
-    output_file_name, deNada = os.path.splitext(output_file_name)
-    output_file_name = os.path.join(run_parameters['results_directory'], output_file_name)
-    output_file_name = kn.create_timestamped_filename(output_file_name) + '.tsv'
 
-    predict_df.to_csv(output_file_name, sep='\t', header=True, index=True)
+    test_spreadsheet_name_full_path = run_parameters['test_spreadsheet_name_full_path']
+    results_directory               = run_parameters['results_directory']
+    method                          = run_parameters['method']
+    _, output_file_name             = os.path.split(test_spreadsheet_name_full_path)
+
+    output_file_name, _             = os.path.splitext(output_file_name)
+    output_file_name                = os.path.join(results_directory, output_file_name + '_' + method)
+    output_file_name                = kn.create_timestamped_filename(output_file_name) + '.tsv'
+
+    predict_df.to_csv(output_file_name, sep='\t', header=True, index=True, float_format='%g')
+
